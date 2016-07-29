cc6808815fc53b1df937315427af8dd7a3b7b7e2
root <root@localhost.localdomain>
2016-07-29 14:36:12 +0100

    new file:   README.TXT 	new file:   package.bluej 	new file:   test.class 	new file:   test.ctxt 	new file:   test.java

---
+++ test.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class test
+{
+    
+    public test()
+    {
+    }
+    
+    public static void main(String [] args)
+    {
+        System.out.println("test");
+    }
+}
