d16d0aa8efaf5f9a4f781cff019d14e6323ca640
root <root@localhost.localdomain>
2016-08-02 13:59:14 +0100

    modified:   test2.java

--- src/test2.java
+++ src/test2.java
@@ -6,24 +6,27 @@ class test2
 {
 	public test2(){}
 
-	public static void main(String[] args) {
-        try {
-            final ServerSocket serverSocket = new ServerSocket(7777);
-            new Thread("Device Listener") {
-                public void run() {
-                    try {
-                        System.out.println("Listener Running . . .");
-                        Socket socket = null;
-                        while ((socket = serverSocket.accept()) != null) {
-                            System.out.println("| Incoming : "+ socket.toString());
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                };
-            }.start();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+	public static void main(String[] args){
+        	try{
+            		final ServerSocket serverSocket = new ServerSocket(23);
+            		new Thread("BBS Server"){
+                		public void run(){
+                    			try{
+                        			System.out.println("BBS Server Running . . .");
+                        			Socket socket = null;
+                        
+						while((socket = serverSocket.accept()) != null){
+                            				System.out.println("Incoming : " + socket.toString());
+                        			}
+                    			}
+					catch(Exception e){
+                        			e.printStackTrace();
+                    			}
+                		};
+            		}.start();
+        	}
+		catch(Exception ex){
+            		ex.printStackTrace();
+        	}
+    	}
 }
