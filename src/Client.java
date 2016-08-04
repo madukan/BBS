bcdc878b7b771a84b7dc819bde09d7c3867f082e
root <root@localhost.localdomain>
2016-08-04 21:20:26 +0100

    modified:   Client.java

--- src/Client.java
+++ src/Client.java
@@ -9,10 +9,10 @@ public class Client{
     	public Client(String hostName, int ip){
         	try{
             		this.client = new Socket(hostName, ip);
-            		this.in = new BufferedReader(new InputStreamReader(
-                    		this.client.getInputStream()));
-            		String buffer = null;
-            
+            		this.in = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
+            		
+			String buffer = null;
+            		
 			while((buffer = in.readLine()) != null){
                 		System.out.println(buffer);
             		}
