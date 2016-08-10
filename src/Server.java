08dfc6db37e2339b0e3e40c58e2fcca81a1a31de
root <damien.hunter9@gmail.com>
2016-08-10 17:37:32 +0100

    modified:   Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,66 +1,55 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
  
 public class Server{
-    private static Socket socket;
+	private static Socket socket;
  
-    public static void main(String[] args){
-        try{
-            ServerSocket serverSocket = new ServerSocket(1025);
-            System.out.println("Server Started and listening to the port 25000");
+    	public static void main(String[] args){
+        	try{
+            		ServerSocket serverSocket = new ServerSocket(1025);
+            		System.out.println("Server Started and listening to the port 25000");
  
-            //Server is running always. This is done using this while(true) loop
-            while(true)
-            {
-                //Reading the message from the client
-                socket = serverSocket.accept();
-                InputStream is = socket.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String number = br.readLine();
+            		// Server is running always. This is done using this while(true) loop
+            		while(true){
+                		//Reading the message from the client
+                		socket = serverSocket.accept();
+                		InputStream is = socket.getInputStream();
+                		InputStreamReader isr = new InputStreamReader(is);
+                		BufferedReader br = new BufferedReader(isr);
+                		String number = br.readLine();
                 
-		System.out.println("Message received from client is " + number);
+				System.out.println("Message received from client is " + number);
  
-                //Multiplying the number by 2 and forming the return message
-                String returnMessage;
-                try
-                {
-                    int numberInIntFormat = Integer.parseInt(number);
-                    int returnValue = numberInIntFormat*2;
-                    returnMessage = String.valueOf(returnValue) + "\n";
-                }
-                catch(NumberFormatException e)
-                {
-                    //Input was not a number. Sending proper message back to client.
-                    returnMessage = "Please send a proper number\n";
-                }
+                		//Multiplying the number by 2 and forming the return message
+                		String returnMessage;
+                
+				try{
+                    			int numberInIntFormat = Integer.parseInt(number);
+                    			int returnValue = numberInIntFormat*2;
+                    			returnMessage = String.valueOf(returnValue) + "\n";
+                		}
+                		catch(NumberFormatException e){
+                    			//Input was not a number. Sending proper message back to client.
+                    			returnMessage = "Please send a proper number\n";
+                		}
  
-                //Sending the response back to the client.
-                OutputStream os = socket.getOutputStream();
-                OutputStreamWriter osw = new OutputStreamWriter(os);
-                BufferedWriter bw = new BufferedWriter(osw);
-                bw.write(returnMessage);
-                System.out.println("Message sent to the client is " + returnMessage);
-                bw.flush();
-            }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
-                socket.close();
-            }
-            catch(Exception e){}
-        }
-    }
+                		//Sending the response back to the client.
+                		OutputStream os = socket.getOutputStream();
+                		OutputStreamWriter osw = new OutputStreamWriter(os);
+               	 		BufferedWriter bw = new BufferedWriter(osw);
+                		bw.write(returnMessage);
+                		System.out.println("Message sent to the client is " + returnMessage);
+                		bw.flush();
+            		}
+        	}
+        	catch (Exception e){
+            		e.printStackTrace();
+        	}
+        	finally{
+            		try{
+                		socket.close();
+            		}
+            		catch(Exception e){}
+        	}
+    	}
 }
