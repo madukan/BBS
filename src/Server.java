04f0e0ef3ad6588b2c299f3634b66ca97b32cbf4
Damien <damien@localhost.localdomain>
2016-08-10 16:04:34 +0100

    deleted:    Client.java

--- src/Server.java
+++ src/Server.java
@@ -1,36 +1,66 @@
-import java.net.*;
-import java.util.*;
-
-public class Server extends Thread{
-	private ServerSocket server;
-    protected List<ClientHandler> clients;
-
-    public Server(int port){
-    	try{
-    		this.server = new ServerSocket(port);
-    		System.out.println("BBS Server has been Initialized!");
-    		
-    		clients = Collections.synchronizedList(new ArrayList<ClientHandler>());
-    		this.start();
-    	}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-    }
-    
-	public void run(){
-		while(true){
-			try{
-				Socket client = server.accept();
-				System.out.println(client.getInetAddress().getHostName() + " Connected");
-				
-				ClientHandler newClient = new ClientHandler(client);
-				clients.add(newClient);
-				new SendMessage(clients);
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+ 
+public class Server{
+    private static Socket socket;
+ 
+    public static void main(String[] args){
+        try{
+            ServerSocket serverSocket = new ServerSocket(1025);
+            System.out.println("Server Started and listening to the port 25000");
+ 
+            //Server is running always. This is done using this while(true) loop
+            while(true)
+            {
+                //Reading the message from the client
+                socket = serverSocket.accept();
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String number = br.readLine();
+                
+		System.out.println("Message received from client is " + number);
+ 
+                //Multiplying the number by 2 and forming the return message
+                String returnMessage;
+                try
+                {
+                    int numberInIntFormat = Integer.parseInt(number);
+                    int returnValue = numberInIntFormat*2;
+                    returnMessage = String.valueOf(returnValue) + "\n";
+                }
+                catch(NumberFormatException e)
+                {
+                    //Input was not a number. Sending proper message back to client.
+                    returnMessage = "Please send a proper number\n";
+                }
+ 
+                //Sending the response back to the client.
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+                bw.write(returnMessage);
+                System.out.println("Message sent to the client is " + returnMessage);
+                bw.flush();
             }
-			catch(Exception e){
-				e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                socket.close();
             }
+            catch(Exception e){}
         }
     }
 }
--- src/initialMessage.java
+++
@@ -1,5 +0,0 @@
-public class initialMessage{
-	public initialMessage(){
-		
-	}
-}
