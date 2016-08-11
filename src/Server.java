c1009f60bc9fe32e91e3c6d0df89cd227d432e6d
root <damien.hunter9@gmail.com>
2016-08-11 17:54:08 +0100

    modified:   Server.java

--- src/Server.java
+++ src/Server.java
@@ -8,29 +8,47 @@ import java.io.*;
  * Damien Hunter (11/08/2016)
  *
  **/
-public class Server{
-	public static void main(String[] args) throws Exception{
-		ServerSocket sersock = new ServerSocket(1025);
-		System.out.println("BBS Server Initialised!");
+public class Server extends Thread{
+	private ServerSocket sersock;
+	private Socket sock;
+	private int port = 0;
 
-		Socket sock = sersock.accept();
-		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-		OutputStream ostream = sock.getOutputStream();
-		PrintWriter pwrite = new PrintWriter(ostream, true);
+	public Server(){}
 
-		// Receiving
-		InputStream istream = sock.getInputStream();
-		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+	public Server(int port){
+		this.port = port;
+		run();
+	}
+
+	public void run(){
+		try{
+			sersock = new ServerSocket(port);
+                	System.out.println("BBS Server Initialised!");
+
+                	sock = sersock.accept();
+                	BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+                	OutputStream ostream = sock.getOutputStream();
+                	PrintWriter pwrite = new PrintWriter(ostream, true);
+
+                	// Receiving
+                	InputStream istream = sock.getInputStream();
+                	BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
  
-		String receiveMessage, sendMessage;
+                	String receiveMessage, sendMessage;
 
-		while(true){
-			if((receiveMessage = receiveRead.readLine()) != null){
-				System.out.println(receiveMessage);
-			}
-			sendMessage = keyRead.readLine();
-			pwrite.println(sendMessage);
-			pwrite.flush();
+                	while(true){
+                        	if((receiveMessage = receiveRead.readLine()) != null){
+                                	System.out.println(receiveMessage);
+                        	}
+                        	sendMessage = keyRead.readLine();
+                        	pwrite.println(sendMessage);
+                        	pwrite.flush();
+                	}
 		}
+		catch(Exception e){}
+	}
+
+	public static void main(String[] args) throws Exception{
+		Server a = new Server(1025);
 	}
 }
